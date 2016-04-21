1010fbabe254e48cf64610ace9bc741087591a95
kusabashira <kusabashira227@gmail.com>
2016-04-21 18:57:30 +0900

    s/Productor.FindNext/Productor.FindProduct/

--- product.go
+++ product.go
@@ -14,7 +14,7 @@ func NewProductor(items [][]string, ch chan []int) *Productor {
 	}
 }
 
-func (p *Productor) FindNext(index_i int) {
+func (p *Productor) FindProduct(index_i int) {
 	if index_i == len(p.items) {
 		indexes := make([]int, len(p.indexes))
 		copy(indexes, p.indexes)
@@ -24,7 +24,7 @@ func (p *Productor) FindNext(index_i int) {
 
 	for i := 0; i < len(p.items[index_i]); i++ {
 		p.indexes[index_i] = i
-		p.FindNext(index_i + 1)
+		p.FindProduct(index_i + 1)
 	}
 }
 
@@ -32,7 +32,7 @@ func Product(items [][]string) chan []int {
 	ch := make(chan []int, 16)
 	go func() {
 		p := NewProductor(items, ch)
-		p.FindNext(0)
+		p.FindProduct(0)
 		close(p.ch)
 	}()
 	return ch
